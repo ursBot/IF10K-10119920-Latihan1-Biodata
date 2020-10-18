14e1fd25b884531589c1034939b89fabd8bb9988
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 16:59:43 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan1/biodata/Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Biodata {
+    public static void main(String[] args) {
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan  Antapani no 89");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
